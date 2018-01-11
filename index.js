27b71548babe53d102277bcce8e0232f87012c33
Anish Kumar <anish000kumar@gmail.com>
2018-01-12 01:26:28 +0530

    Resolved bugs

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
 "use strict";
-
 import React from 'react';
-import {applyMiddleware, combineReducers, compose, createStore as storeCreator} from 'redux';
-import {createLogger} from 'redux-logger';
+import {applyMiddleware,combineReducers, compose, createStore as storeCreator} from 'redux';
 import createSagaMiddleware from "redux-saga";
 import { connect } from 'react-redux';
+import { all } from 'redux-saga/effects';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSIONS_COMPOSE__ || compose;
 const sagaMiddleware = createSagaMiddleware();
 let middlewares = [sagaMiddleware];
 
-if (__DEV__) {
-    middlewares = [
-        ...middlewares,
-        createLogger(), 
-    ];
-}
 
-const getReducer = (actionList, initialState) => {
+export const getReducer = (actionList, initialState) => {
 	return  ( state = initialState, action) => {
 		let method = actionList[action.type];
 		let clone_state = JSON.parse(JSON.stringify(state))
@@ -26,46 +19,37 @@ const getReducer = (actionList, initialState) => {
 	}
 }
 
-
-let reducers = {};
-
-module.exports = {
-	createContainer : (attrs) =>{
-		let container =  connect(
-		 state => state[attrs.name],
-		 attrs.actions,
-		)( (props) => props.children(props) );
-
-		return {
-			container,
-			attrs
-		}
-
-	},
-
-	createStore : (containers, new_middlewares) => {
-		let saga_array = [];
-		  Object.keys(containers).forEach( key => {
-			let module = containers[key].attrs;
-			saga_array.concat(module.sagas)
-			reducers[key] = getReducer( module.mutations , module.state ) 
-		})
-
-		// function* rootSaga(){
-		// 	yield all(saga_array)
-		// }
-
-		middlewares = middlewares.concat(new_middlewares)
-		let store = storeCreator(
-		    combineReducers(reducers),
-		    composeEnhancers(
-		        applyMiddleware(...middlewares)
-		    )
-		);
-
-		// sagaMiddleware.run(rootSaga)
-		return store;
+export const createContainer = (module) =>{
+	const mapStateToProps = state => state[module.name]
+	const Container = (props) => props.children(props);
+	return connect(
+		mapStateToProps,
+		module.actions,
+	)(Container);
+}
+	
+export const createStore = (modules, new_middlewares) => {
+	middlewares = middlewares.concat(new_middlewares);
+	let reducerList = {}
+	let sagas = [];
+	modules.forEach(module => {
+		sagas = sagas.concat(module.sagas);
+		reducerList[module.name] = getReducer( module.mutations, module.state ) 		
+	})
+	
+	let store = storeCreator(
+		combineReducers(reducerList),
+		composeEnhancers( applyMiddleware(...middlewares))
+	)
+	function *rootSaga(){
+		yield all(sagas)
 	}
-
+	sagaMiddleware.run(rootSaga);
+	return store;
 }
 
+export default {
+	createContainer,
+	createStore,
+	getReducer
+}

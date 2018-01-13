84ef48974839c4637ce18e9e10188b801b00c9fc
Anish Kumar <anish000kumar@gmail.com>
2018-01-13 17:54:52 +0530

    Updated the file architecture

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 "use strict";
 import React from 'react';
 import produce from 'immer';
+import { connect } from 'react-redux';
 import {applyMiddleware,combineReducers, compose, createStore as storeCreator} from 'redux';
 import createSagaMiddleware from "redux-saga";
-import {all} from 'redux-saga/effects';
+import {all, takeLatest, takeEvery, put} from 'redux-saga/effects';
 import {assign, createContainer, createSagas} from './helpers';
 import getReducer from './reducer';
 
@@ -53,11 +54,56 @@ export const dispatch = (action_name, data ) =>{
 	})
 }
 
+export const createContainer = (module) =>{
+	const mapStateToProps = state => state[module.name]
+	const set = function( target, value ){
+		STORE.dispatch({
+			type : module.name+'__SET',
+			data :{
+				target,
+				value
+			}
+		})
+	}
+	const Container = (props) => props.children( Object.assign({}, props, {
+		 dispatch : dispatch,
+		 commit : commit,
+		 set: set
+	}) );
+	return connect(
+		mapStateToProps,{}
+	)(Container);
+}
+
+
+export const createSagas = (saga_list) => {
+	let arr = [];
+	var GeneratorFunction = Object.getPrototypeOf(function*(){}).constructor
+	let saga_keys = Object.keys(saga_list);
+	saga_keys.forEach( key => {
+		let action = key.split('.')[0];
+		let worker_saga = saga_list[key];
+		let mode = key.split('.')[1];
+		let watcher = null;
+		if(mode=='latest'){
+			watcher = function* (){
+				yield takeLatest(action, worker_saga)
+			}
+		}
+		else if(mode == 'every'){
+			watcher = function* (){
+				yield takeEvery(action, worker_saga)
+			}
+		}
+		arr.push(watcher())
+	})
+	return arr;
+}
+
 export default {
 	createContainer,
 	createSagas,
 	createStore,
-	getReducer,
 	dispatch,
 	commit
 }

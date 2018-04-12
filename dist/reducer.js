e8ad256096b670b71677c8a4ec86f137324a55de
Anish Kumar <anish000kumar@gmail.com>
2018-04-12 17:02:52 +0530

    improved resetModules

--- dist/reducer.js
+++ dist/reducer.js
@@ -1,47 +1,16 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
 	value: true
 });
 
-var _immer = require('immer');
+var _immer = require("immer");
 
 var _immer2 = _interopRequireDefault(_immer);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-// const assign = (obj, prop, value) => {
-//     if (typeof prop === "string")
-// 		prop = prop.split(".");
-
-//     if (prop.length > 1) {
-//         var e = prop.shift();
-//         assign(obj[e] , prop, value);
-//     } else
-//         obj[prop[0]] = value;
-//   return obj
-// }
-
 var getReducer = function getReducer(actionList, initialState, name) {
-	// actionList['__SET__'+name] = function(state, {data}){
-	// 	try{
-	// 		 assign(state, data.target, data.value )
-	// 	}
-	// 	catch(err){
-	// 		console.log('WARNING: the key specified for the setter wasn\'t valid', err)
-	// 	}
-	// }
-
-	// actionList[name+'__RESET__'] = function(state, action){
-	// 	try{
-	// 		state = initialState;
-	// 		return initialState
-	// 	}
-	// 	catch(err){
-	// 		console.log(`WARNING: __RESET__ action failed for module ${name}`,err)
-	// 	}
-	// }
-
 	return function () {
 		var state = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : initialState;
 		var action = arguments[1];
--- src/reducer.js
+++ src/reducer.js
@@ -1,37 +1,6 @@
-import produce from 'immer';
- 
-// const assign = (obj, prop, value) => {
-//     if (typeof prop === "string")
-// 		prop = prop.split(".");
+import produce from "immer";
 
-//     if (prop.length > 1) {
-//         var e = prop.shift();
-//         assign(obj[e] , prop, value);
-//     } else
-//         obj[prop[0]] = value;
-//   return obj
-// }
-
- const getReducer = (actionList, initialState, name) => {
-	// actionList['__SET__'+name] = function(state, {data}){
-	// 	try{
-	// 		 assign(state, data.target, data.value )
-	// 	}
-	// 	catch(err){
-	// 		console.log('WARNING: the key specified for the setter wasn\'t valid', err)
-	// 	}
-	// }
-
-	// actionList[name+'__RESET__'] = function(state, action){
-	// 	try{
-	// 		state = initialState;
-	// 		return initialState
-	// 	}
-	// 	catch(err){
-	// 		console.log(`WARNING: __RESET__ action failed for module ${name}`,err)
-	// 	}
-	// }
-	
+const getReducer = (actionList, initialState, name) => {
 	return  ( state = initialState, action) => {
 		let method = actionList[action.type];
 		if(action.type== name+'__RESET__'){
@@ -43,7 +12,8 @@ import produce from 'immer';
 			})
 			return nextState;
 		}
-		else return state
+		else 
+			return state
 	}
-}
-export default getReducer
+};
+export default getReducer;
